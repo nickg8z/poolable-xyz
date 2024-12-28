ecf1e23817f0d3840fba4317445c0642263979cd
anonymous <anonymous@users.noreply.github.com>
2024-12-28 21:25:47 +0700

    anotha one

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#42de95",
+          light: "#c1e5ea",
+          dark: "#2e2c3b",
+        },
+        secondary: {
+          DEFAULT: "#26969c",
+          light: "#c1e5ea",
+          dark: "#2e2c3b",
+        },
+      },
+      fontFamily: {
+        retro: ["Retropix", "sans-serif"],
+        comp: ["Computer-Says-No", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
